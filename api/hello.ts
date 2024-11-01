3a4974cd9b89b86dc3decf7d3bc2c93f42195610
omkar-4 <omkar.pro04@gmail.com>
2024-11-01 21:15:23 +0530

    .

--- api/hello.ts
+++ api/hello.ts
@@ -1,47 +1,49 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
-import mongoose, { Document } from "mongoose";
+console.log("endpoint is working");
 
-// TypeScript interface for data model
-interface IData extends Document {
-  name: string;
-  value: number;
-}
+// import { VercelRequest, VercelResponse } from "@vercel/node";
+// import mongoose, { Document } from "mongoose";
 
-// Mongoose schema and model
-const dataSchema = new mongoose.Schema<IData>({
-  name: { type: String, required: true },
-  value: { type: Number, required: true },
-});
+// // TypeScript interface for data model
+// interface IData extends Document {
+//   name: string;
+//   value: number;
+// }
 
-const DataModel = mongoose.model<IData>("Data", dataSchema);
+// // Mongoose schema and model
+// const dataSchema = new mongoose.Schema<IData>({
+//   name: { type: String, required: true },
+//   value: { type: Number, required: true },
+// });
 
-// MongoDB connection URI
-const uri = process.env.MONGODB_URI;
+// const DataModel = mongoose.model<IData>("Data", dataSchema);
 
-// A singleton for MongoDB connection
-async function connectToDatabase() {
-  if (!uri) {
-    throw new Error("MongoDB URI is not defined");
-  }
-  if (mongoose.connection.readyState === 0) {
-    await mongoose.connect(uri);
-  }
-}
+// // MongoDB connection URI
+// const uri = process.env.MONGODB_URI;
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  try {
-    await connectToDatabase();
+// // A singleton for MongoDB connection
+// async function connectToDatabase() {
+//   if (!uri) {
+//     throw new Error("MongoDB URI is not defined");
+//   }
+//   if (mongoose.connection.readyState === 0) {
+//     await mongoose.connect(uri);
+//   }
+// }
 
-    const data = await DataModel.find({}).catch((err) => {
-      console.error("Error fetching data:", err);
-      throw new Error("Error fetching data");
-    });
+// export default async function handler(req: VercelRequest, res: VercelResponse) {
+//   try {
+//     await connectToDatabase();
 
-    res.status(200).json(data);
-  } catch (error) {
-    console.error("Error in API handler:", error); // Log the error for debugging
-    res
-      .status(500)
-      .json({ message: "Internal Server Error", error: error.message });
-  }
-}
+//     const data = await DataModel.find({}).catch((err) => {
+//       console.error("Error fetching data:", err);
+//       throw new Error("Error fetching data");
+//     });
+
+//     res.status(200).json(data);
+//   } catch (error) {
+//     console.error("Error in API handler:", error); // Log the error for debugging
+//     res
+//       .status(500)
+//       .json({ message: "Internal Server Error", error: error.message });
+//   }
+// }
